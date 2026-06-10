807daaf8677952226d47372e6c9bda98e28d577b
Romanchenko Oleksandr <romanchenko.sawa@gmail.com>
2026-06-11 01:08:39 +0300

    fixing push notifications on app - revert back

--- backend/src/utils/firebaseAdmin.js
+++ backend/src/utils/firebaseAdmin.js
@@ -1,8 +1,9 @@
 const serviceAccount = require('../../firebase-service-account.json');
+
 const admin = require('firebase-admin');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.cert(serviceAccount),
 });
 
 module.exports = admin;
